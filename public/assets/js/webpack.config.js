7d8ab3b592b3c3bd7e5c6f2c81d518fd087f9db2
JustN00ne <justnoone963@gmail.com>
2025-05-02 23:47:29 +0200

    .

--- public/assets/js/webpack.config.js
+++ public/assets/js/webpack.config.js
@@ -1,10 +1,28 @@
 const path = require("path");
+const glob = require("glob");
 
 module.exports = {
-  entry: "./public/assets/js/smooth-scroll.js",
+  entry: glob.sync("./public/assets/**/*.js"),
   output: {
-    filename: "smooth-scroll.bundle.js",
-    path: path.resolve(__dirname, "public/assets/js"),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public/assets/dist"),
   },
   mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader", // Optional if using ES6+
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
