be7e875d9e5eae2254bffae75649c3c87176ae08
JustN00ne <justnoone963@gmail.com>
2025-05-11 16:48:33 +0200

    fuck me

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const handlebars = require('express-handlebars');
 const compression = require('compression');
+const request = require("request");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -23,6 +24,38 @@ app.use(express.static(path.join(__dirname, 'public'), {
 
 // Use compression middleware
 app.use(compression());
+
+// Reverse proxy in Node.js that strips frame-blocking headers
+app.use("/proxy-map", (req, res) => {
+  const target = "http://104.36.84.122:8181" + req.url;
+  request(target, {
+    headers: {
+      // Pretend we're a normal browser
+      "User-Agent": "Mozilla/5.0"
+    }
+  })
+    .on("response", response => {
+      delete response.headers["x-frame-options"];
+      delete response.headers["content-security-policy"];
+    })
+    .pipe(res);
+});
+
+
+// Reverse proxy for http://104.36.84.122:8888 (systemmap)
+app.use("/proxy-system", (req, res) => {
+  const target = "http://104.36.84.122:8888" + req.url.replace(/^\/proxy\/systemmap/, "");
+  request(target, {
+    headers: {
+      "User-Agent": "Mozilla/5.0"
+    }
+  })
+    .on("response", response => {
+      delete response.headers["x-frame-options"];
+      delete response.headers["content-security-policy"];
+    })
+    .pipe(res);
+});
 
 // Render any .hbs file in /public by its name (e.g. /gallery -> gallery.hbs)
 app.get('/:page?', (req, res, next) => {
